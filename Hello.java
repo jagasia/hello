8870b97cff175a3678a63b353118d55601402ede
jagasia <46959568+jagasia@users.noreply.github.com>
2025-09-09 08:12:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
 	public static void main(String []args)
 	{
-		System.out.println("Hello this is UST Wave 4 batch...");
+		System.out.println("Hello this is Empower 2025 batch");
 		System.out.println("I have modified this program so that ");
 		System.out.println("These changes are now reflecting when jenkins run");
 	}
